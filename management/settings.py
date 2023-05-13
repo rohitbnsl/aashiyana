ff94369760b6795e009600a5f7a6eea373c521b4
Rohit <72278696+rohitbnsl@users.noreply.github.com>
2023-05-13 12:50:26 +0530

    Update settings.py

--- management/settings.py
+++ management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4=#t2hyk8k!jkxzv-h)2nz$9%us0*evmr+080q5vaq1l=inqko
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
